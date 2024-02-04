eba9b2c61f6bf491352fd4154cb0a06a64cbe530
abdeljalil yahya <jalil.yahy@gmail.com>
2024-02-04 18:48:04 +0100

    css cleanup with homepage

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/lib", under: "lib"
+
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
