08277cce9bdbb543aee2b491249f263f0fa572e3
abdeljalil yahya <jalil.yahy@gmail.com>
2024-02-05 15:38:51 +0100

    front back photos backend

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_105119) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_05_143235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_02_105119) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "front_photo_url"
+    t.string "back_photo_url"
   end
 
   create_table "machines", force: :cascade do |t|
