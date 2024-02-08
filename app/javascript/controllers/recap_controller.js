a3949fc167718b0602c9a1f9172192c156bf05fb
abdeljalil yahya <jalil.yahy@gmail.com>
2024-02-08 12:45:23 +0100

    edit form to backend

--- app/javascript/controllers/recap_controller.js
+++ app/javascript/controllers/recap_controller.js
@@ -3,11 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="recap"
 export default class extends Controller {
 
+  static targets = ["hiddenForm"]
   connect() {
-    this.hidden_form = document.createElement("form");
-    this.hidden_form.id = "hidden_form"
-    this.hiddenForm.action = "<%= catalog_order_path %>";
-    this.hiddenForm.method = "PATCH"
+    // static values = {
+    //   catalog_id : Number,
+    //   id : Number
+    // };
+
+    // this.hiddenFormTarget = document.createElement("form");
+    // this.hiddenFormTarget.id = "hiddenFormTarget"
+
+    // this.hiddenForm.action = `/catalogs/${catalog_id}/orders/${id}`;
+    // this.hiddenForm.method = "PATCH"
 
   }
   change(element) {
@@ -18,20 +25,36 @@ export default class extends Controller {
 
     });
     element.target.classList.add('active')
-    const hidden_input = document.createElement("input")
-    hidden_input.value = element.target.innerText
-    hidden_input.name = element.target.id.split('_').slice(1).join("_")
-    let nameAttributeValue = hidden_input.getAttribute('name');
-    let existingChild = this.hidden_form.querySelector('[name="' + nameAttributeValue + '"]');
+    // const hidden_input = document.createElement("input")
+    // hidden_input.value = element.target.innerText
+    // hidden_input.name = element.target.id.split('_').slice(1).join("_")
+    // let nameAttributeValue = hidden_input.getAttribute('name');
+    // let existingChild = this.hiddenFormTarget.querySelector('[name="' + nameAttributeValue + '"]');
 
-    if (existingChild) {
-        this.hidden_form.replaceChild(hidden_input, existingChild);
+    // if (existingChild) {
+    //     this.hiddenFormTarget.replaceChild(hidden_input, existingChild);
+    // }
+    // else {
+    //     this.hiddenFormTarget.appendChild(hidden_input);
+    // }
+    // console.log(element.target.innerText)
+    // console.log(hidden_input.value)
+    // console.log(this.hiddenFormTarget)
+
+    const name = "order[" + element.target.id.split('_').slice(1).join("_") + "]"
+    const value = element.target.innerText || element.target.value
+    const existingInput = this.hiddenFormTarget.querySelector('[name="' + name + '"]');
+    if (existingInput) {
+      existingInput.value = value
+      return
     }
-    else {
-        this.hidden_form.appendChild(hidden_input);
-    }
-    console.log(element.target.innerText)
-    console.log(hidden_input.value)
-    console.log(this.hidden_form)
+    const newInput = document.createElement("input")
+    newInput.name = name
+    newInput.value = value
+    this.hiddenFormTarget.appendChild(newInput)
+  }
+
+  submitForm() {
+    this.hiddenFormTarget.submit()
   }
 }
